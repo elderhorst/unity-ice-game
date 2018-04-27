abf5effacbed1df1e7e43fccc6a8e10f4adaa2f0
Michael Elderhorst <michael.elderhorst@gmail.com>
2018-04-27 15:20:29 -0400

    Add action to translate objects

--- Assets/Scripts/Actions/ActionManager.cs
+++ Assets/Scripts/Actions/ActionManager.cs
@@ -30,5 +30,51 @@ namespace Actions {
 
 			doneCallback();
 		}
+
+		public static IEnumerator fadeTransition(bool fadeIn, SpriteRenderer sprite, System.Action doneCallback) {
+
+			float start = (fadeIn) ? 0 : 1;
+			float end = (fadeIn) ? 1 : 0;
+
+			float currentTime = 0;
+			float duration = 0.3f;
+
+			while (currentTime <= duration) {
+
+				Color color = sprite.color;
+				color.a = Mathf.Lerp(start, end, currentTime / duration);
+
+				sprite.color = color;
+
+				currentTime += Time.deltaTime;
+
+				yield return null;
+			}
+
+			doneCallback();
+		}
+
+		public static IEnumerator translateObject(GameObject obj, Vector3 start, Vector3 end, float duration, System.Action doneCallback = null) {
+
+			float currentTime = 0;
+
+			while (currentTime <= duration) {
+
+				float x = Mathf.Lerp(start.x, end.x, currentTime / duration);
+				float y = Mathf.Lerp(start.y, end.y, currentTime / duration);
+				float z = Mathf.Lerp(start.z, end.z, currentTime / duration);
+
+				obj.transform.localPosition = new Vector3(x, y, z);
+
+				currentTime += Time.deltaTime;
+
+				yield return null;
+			}
+
+			if (doneCallback != null) {
+
+				doneCallback();
+			}
+		}
 	}
 }
